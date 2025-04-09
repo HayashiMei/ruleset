b36a47b91160f10a9a6103b4f394f4411dd88ad3
SukkaBot <githubbot@skk.moe>
2025-04-09 06:43:17 +0000

    deploy: https://github.com/SukkaW/Surge/commit/b32e8b11aac1eb1496397178c31b120c52016332

--- Internal/appprofile.php
+++ Internal/appprofile.php
@@ -1,6 +1,6 @@
 <?php
 
-// # Build 2025-04-09T04:01:43.466Z
+// # Build 2025-04-09T06:42:28.493Z
 
 declare(strict_types=1);
 
@@ -2178,6 +2178,7 @@ $_ENV['Clash_Group_Config'] = [
         'DOMAIN-SUFFIX,cnn.com,Global',
         'DOMAIN-SUFFIX,cbs.com,Global',
         'DOMAIN-SUFFIX,cal.com,Global',
+        'DOMAIN-SUFFIX,box.com,Global',
         'DOMAIN-SUFFIX,bbc.com,Global',
         'DOMAIN-SUFFIX,avg.com,Global',
         'DOMAIN-SUFFIX,ask.com,Global',
@@ -4883,15 +4884,14 @@ $_ENV['Clash_Group_Config'] = [
         'IP-CIDR,120.72.32.0/19,Domestic',
         'IP-CIDR,120.72.160.0/24,Domestic',
         'IP-CIDR,120.72.162.0/23,Domestic',
-        'IP-CIDR,120.72.164.0/22,Domestic',
+        'IP-CIDR,120.72.166.0/23,Domestic',
         'IP-CIDR,120.72.168.0/24,Domestic',
         'IP-CIDR,120.72.170.0/23,Domestic',
         'IP-CIDR,120.72.172.0/23,Domestic',
-        'IP-CIDR,120.72.174.0/24,Domestic',
         'IP-CIDR,120.72.177.0/24,Domestic',
         'IP-CIDR,120.72.178.0/24,Domestic',
         'IP-CIDR,120.72.180.0/24,Domestic',
-        'IP-CIDR,120.72.182.0/23,Domestic',
+        'IP-CIDR,120.72.182.0/24,Domestic',
         'IP-CIDR,120.72.184.0/22,Domestic',
         'IP-CIDR,120.72.188.0/23,Domestic',
         'IP-CIDR,120.72.190.0/24,Domestic',
